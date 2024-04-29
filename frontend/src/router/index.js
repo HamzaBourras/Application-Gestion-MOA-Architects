5677207e0f1beded6f3bd97a3c4683e3366b0b04
HamzaBourras <bourrashamza2004@gmail.com>
2024-04-29 12:35:09 +0000

    commit 1

---
+++ frontend/src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes = [];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
