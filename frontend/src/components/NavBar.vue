8b5d733ab9af99035d68f36b6112c0e73d2d27ce
HamzaBourras <bourrashamza2004@gmail.com>
2024-05-02 14:36:54 +0000

    navbar style 3

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -5,25 +5,26 @@
         <img class="w-24 cursor-pointer" src="@/assets/logo1.png" alt="...">
       </div>
       <div ref="navLinks"
-        class="text-[19px] duration-500 md:static md:bg-inherit bg-white md:shadow-none shadow-md absolute md:min-h-fit min-h-[60vh] right-0 top-[113px] md:w-auto  w-[45vw] flex items-center justify-center md:pt-0 pt-10 px-5">
+        class="text-[17.5px] duration-500 md:static md:bg-inherit bg-white md:shadow-none shadow-md absolute md:min-h-fit min-h-[60vh] right-0 top-[113px] md:w-auto  w-[45vw] flex items-center justify-center md:pt-0 pt-10 px-5">
         <ul class="flex md:flex-row flex-col md:items-center justify-start md:gap-[4vw] gap-8">
           <li>
-            <span class="inline-block rounded text-white py-1 cursor-pointer">Accueil</span>
+            <span class="inline-block rounded text-[#0000008e] py-1 cursor-pointer">Accueil</span>
           </li>
           <li>
-            <span class="inline-block rounded text-white py-1 cursor-pointer">Experiences</span>
+            <span class="inline-block rounded text-[#0000008e] py-1 cursor-pointer">Experiences</span>
           </li>
           <li>
-            <span class="inline-block rounded text-white  py-1 cursor-pointer">Contactez-nous</span>
+            <span class="inline-block rounded text-[#0000008e]  py-1 cursor-pointer">Contactez-nous</span>
           </li>
           <li>
-            <span class="inline-block rounded text-white py-1 cursor-pointer">à propos de nous</span>
+            <span class="inline-block rounded text-[#0000008e] py-1 cursor-pointer">à propos de nous</span>
           </li>
         </ul>
       </div>
       <div class="flex items-center gap-6">
         <div>
-          <button class="border bg-[#ffffff83] text-white hover:bg-[#526D82] px-5 py-2 rounded-xl text-[16px]">Se
+          <button
+            class="border bg-[#303ef7] text-white hover:bg-blue-800 px-5 py-2 rounded-xl text-[16px] focus:ring-blue-500 focus:ring-2">Se
             connecter</button>
         </div>
         <ion-icon @click="onToggleMenu(this)" name="menu" class="text-3xl cursor-pointer md:hidden"></ion-icon>
@@ -50,7 +51,7 @@ li span {
 }
 li span::after {
   content: '';
-  background: #222831;
+  background: #303ef7;
   position: absolute;
   bottom: 0;
   left: 0;
@@ -60,7 +61,7 @@ li span::after {
 }
 
 li span:hover {
-  color: rgb(191, 191, 191)
+  color: black
 }
 
 li span:hover::after {
